32958803367d4d5dc597e4a8db1ba391e88c9302
Lucas Arruda <99208855+devlucasarruda@users.noreply.github.com>
2022-12-31 10:33:46 -0300

    Criação da Função matricular e de conferir os alunos matriculados, e outros ajustes no código.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,46 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { INICIANTE, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario (val nome: String, val id: Int, var email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, var duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("O aluno ${usuario.nome} foi matriculado na formação de $nome.")
+    }
+    
+    fun conferirAlunosMatriculados(){
+        println("Os alunos matriculados na Formação de $nome são: ")
+        for(aluno in inscritos){  
+        	println(aluno.nome)  
+    	}  
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val logicPro = ConteudoEducacional("Lógica de Programação")
+    val introKotlin = ConteudoEducacional("Introdução ao Kotlin", 45)
+    
+    val listConteudosKotlin = listOf(logicPro, introKotlin)
+    
+    val Kotlin = Formacao("Kotlin", listConteudosKotlin)
+    
+    val Lucas = Usuario("Lucas", 123, "lucas@email.com")
+    val Falvo = Usuario("Falvo", 456, "falvo@algumemail.com")
+
+    
+    Kotlin.matricular(Lucas)
+    Kotlin.matricular(Falvo)
+    
+    Kotlin.conferirAlunosMatriculados()
 }
